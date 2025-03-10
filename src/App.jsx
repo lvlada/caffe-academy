ef75fde1f02b505b7b38efb538c5bbf38fecbe5b
lvlada <vladimirlukic197@gmail.com>
2025-03-10 17:28:40 +0100

    Firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HomePage } from './components/HomePage';
-import { Login } from './components/Login';
-import { Profile } from './components/Profile';
-import { AdminDashboard } from './components/AdminDashboard';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/HomePage";
+import { Login } from "./components/Login";
+import { Profile } from "./components/Profile";
+import { AdminDashboard } from "./components/AdminDashboard";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -14,24 +14,25 @@ function App() {
   function handleLogin(loginState, logedUser) {
     setIsLogin(loginState);
     setUser(logedUser);
-    
   }
+
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage 
-          checkLogin={handleLogin}
-          isLogin={isLogin}
-          userLogin={handleLogin}
-          user={user}
-        />} />
-        <Route path="/login" element={<Login 
-           checkLogin={handleLogin} 
-        />} />
-        <Route path="/profile" element={<Profile 
-           user={user} 
-        />} />
+        <Route
+          path="/"
+          element={
+            <HomePage
+              checkLogin={handleLogin}
+              isLogin={isLogin}
+              userLogin={handleLogin}
+              user={user}
+            />
+          }
+        />
+        <Route path="/login" element={<Login checkLogin={handleLogin} />} />
+        <Route path="/profile" element={<Profile user={user} />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
