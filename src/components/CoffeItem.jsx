dba5583ea748eeeb449508153a49fd2936a394a7
lvlada <vladimirlukic197@gmail.com>
2025-03-14 01:02:35 +0100

    coffe-item update

--- src/components/CoffeItem.jsx
+++ src/components/CoffeItem.jsx
@@ -2,37 +2,58 @@ import { useState } from "react";
 import cofeImage from "../assets/coffeImage.png";
 import Collapse from "react-bootstrap/Collapse";
 
-export function CoffeItem({coffe}) {
+export function CoffeItem({ coffe }) {
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <div className="coffe-item">
         <div className="coffe-item-top">
-          <div className="coffe-item-left">
-            <img src={cofeImage} alt="coffe-image" className="coffe-image" />
+          <div className="coffe-item-left" >
+            <img src={cofeImage} alt="coffe-image" className={open ? null : "coffe-image"} />
           </div>
           <div className="coffe-item-right">
+            <div className="coffe-item-right-title">
             <h6>
               <strong>{coffe?.name}</strong>
-            </h6>
+              </h6>
+              {open ? (
+                <>
+                  <span>
+                    <strong>{coffe?.price} RSD</strong>
+                  </span>
+                </>
+              ) : null}
+            </div>
             <p className="caffe-description">{coffe?.description}</p>
             <div className="coffe-item-right-footer">
-              <div className="coffe-item-right-footer-price">
-                <p>
-                  Cena:
-                  <br />
-                  <span><strong>{coffe?.price} RSD</strong></span>
-                </p>
-              </div>
-              <button
-                className="coffe-item-button"
-                onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open}
-              >
-                <i className="fas fa-plus"></i>
-              </button>
+              {open ? (
+                <div className="coffe-item-right-footer-coffe-size">
+                  <img src="/S-size.png" alt="Coffe size S" />
+                  <img src="/M-size.png" alt="Coffe size M" />
+                  <img src="/L-size.png" alt="Coffe size L" />
+                </div>
+              ) : (
+                <>
+                  <div className="coffe-item-right-footer-price">
+                    <p>
+                      Cena:
+                      <br />
+                      <span>
+                        <strong>{coffe?.price} RSD</strong>
+                      </span>
+                    </p>
+                  </div>
+                  <button
+                    className="coffe-item-button"
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                  >
+                    <i className="fas fa-plus"></i>
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
@@ -42,31 +63,29 @@ export function CoffeItem({coffe}) {
             <div id="example-collapse-text">
               <div className="coffe-item-dropdowns">
                 <div className="dropdown-left">
-
                   <fieldset>
-                  <legend>Zrno</legend>
-                  <select id="dropdown-left" className="dropdown">
-                    <option value="" selected className="option-placeholder">
-                      Odaberi zrno
-                    </option>
-                    <option value="option1">Brazil</option>
-                    <option value="option2">Kuba</option>
-                    <option value="option2">Etiopija</option>
-                  </select>
+                    <legend>Zrno</legend>
+                    <select id="dropdown-left" className="dropdown" required>
+                      <option value="" hidden className="option-placeholder">
+                        Odaberi zrno
+                      </option>
+                      <option value="option1">Brazil</option>
+                      <option value="option2">Kuba</option>
+                      <option value="option2">Etiopija</option>
+                    </select>
                   </fieldset>
-
                 </div>
                 <div className="dropdown-right">
                   <fieldset>
-                  <legend>Mleko</legend>
-                  <select id="dropdown-right" className="dropdown">
-                    <option value="" selected className="option-placeholder">
-                      Odaberi mleko
-                    </option>
-                    <option value="option1">Obicno mleko</option>
-                    <option value="option2">Bademovo mleko</option>
-                    <option value="option2">Sojino mleko</option>
-                  </select>
+                    <legend>Mleko</legend>
+                    <select id="dropdown-right" className="dropdown" required>
+                      <option value="" selected hidden >
+                        Odaberi mleko
+                      </option>
+                      <option value="option1">Obicno mleko</option>
+                      <option value="option2">Bademovo mleko</option>
+                      <option value="option2">Sojino mleko</option>
+                    </select>
                   </fieldset>
                 </div>
               </div>
