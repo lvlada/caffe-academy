c5bbbdb3038cb1fc78bac72afee90c0a6a05c1e8
lvlada <vladimirlukic197@gmail.com>
2025-03-26 15:18:06 +0100

    reg form

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -106,7 +106,7 @@ export function RegisterPage() {
             >
               <input
                 type="text"
-                placeholder="Unesite Ime i prezime"
+                placeholder="Unesite ime i prezime"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
