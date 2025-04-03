0e238f68d13d80113b0263b032f56ada35fea492
lvlada <vladimirlukic197@gmail.com>
2025-04-04 00:25:11 +0200

    loader

--- src/components/CoffeList.jsx
+++ src/components/CoffeList.jsx
@@ -7,11 +7,17 @@ export function CoffeList() {
  
     return (
         <>
-        <div className="coffe-list">
-            {data.map((item) => (
-                <CoffeItem key={item.id} coffe={item} />
-            ))}
-        </div>
+        { data && data.length > 0 ? (
+                    <div className="coffe-list">
+                    {data.map((item) => (
+                        <CoffeItem key={item.id} coffe={item} />
+                    ))}
+                </div>
+        ) : (<div className="loader-container">
+            <span className="loader"></span>
+          </div>)}
+
+  
           
         </>
 
